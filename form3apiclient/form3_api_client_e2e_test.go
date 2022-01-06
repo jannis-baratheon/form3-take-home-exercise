dc7afa84ca0a5c9aee25a901bd819387c9af6bb7
Janusz <janusz@januszwisniowski.it>
2022-01-07 00:39:46 +0100

    Fixing e2e tests

--- form3apiclient/form3_api_client_e2e_test.go
+++ form3apiclient/form3_api_client_e2e_test.go
@@ -1,22 +1,29 @@
 package form3apiclient_test
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/google/uuid"
 	"github.com/jannis-baratheon/Form3-take-home-excercise/form3apiclient"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"net/http"
 )
 
-var _ = PDescribe("Form3ApiClient with real server", Label("e2e"), func() {
+var _ = Describe("Form3ApiClient with real server", Label("e2e"), func() {
 	var accounts form3apiclient.Accounts
+	url := os.Getenv("FORM3_API_URL")
+
+	if url == "" {
+		panic("FORM3_API_URL has to be set")
+	}
 
 	BeforeEach(func() {
-		accounts = form3apiclient.NewForm3APIClient("http://localhost:8080/v1", &http.Client{}).Accounts()
+		accounts = form3apiclient.NewForm3APIClient(url, &http.Client{}).Accounts()
 	})
 
 	It("runs simple pipeline without error", func() {
-		var accountData form3apiclient.AccountData
+		var resource form3apiclient.AccountData
 
 		By("creating an account", func() {
 			accountData := form3apiclient.AccountData{
@@ -29,19 +36,22 @@ var _ = PDescribe("Form3ApiClient with real server", Label("e2e"), func() {
 					Country:               "PL",
 				},
 			}
-			_, err := accounts.Create(accountData)
+			var err error
+			resource, err = accounts.Create(accountData)
 			Expect(err).NotTo(HaveOccurred())
+
+			DeferCleanup(func() {
+				By("and then deleting it", func() {
+					err := accounts.Delete(resource.ID, 0)
+					Expect(err).NotTo(HaveOccurred())
+				})
+			})
 		})
 
 		By("fetching it afterwards", func() {
-			fetchedAccountData, err := accounts.Get(accountData.ID)
+			fetchedAccountData, err := accounts.Get(resource.ID)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(fetchedAccountData).To(Equal(accountData))
-		})
-
-		By("and finally deleting it", func() {
-			err := accounts.Delete(accountData.ID, 0)
-			Expect(err).NotTo(HaveOccurred())
+			Expect(fetchedAccountData).To(Equal(resource))
 		})
 	})
 })
