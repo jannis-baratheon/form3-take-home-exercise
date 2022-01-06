3754c675613490f962b1c7515d93624eb4d00967
Janusz <janusz@januszwisniowski.it>
2022-01-06 21:44:52 +0100

    Adding util tests

--- form3apiclient/util.go
+++ form3apiclient/util.go
@@ -7,16 +7,38 @@ import (
 )
 
 func join(baseAbsoluteUrl string, relativePath string) (string, error) {
-	url, err := url.Parse(baseAbsoluteUrl)
+	baseUrl, err := url.Parse(baseAbsoluteUrl)
 
 	if err != nil {
 		return "", err
 	}
 
-	if !url.IsAbs() {
-		return "", fmt.Errorf("api url must be absolute")
+	if !baseUrl.IsAbs() {
+		return "", fmt.Errorf("baseAbsoluteUrl must be absolute")
 	}
 
-	url.Path = path.Join(url.Path, relativePath)
-	return url.String(), nil
+	if len(baseUrl.Query()) > 0 {
+		return "", fmt.Errorf("baseAbsoluteUrl with query is not supported")
+	}
+
+	if baseUrl.Fragment != "" {
+		return "", fmt.Errorf("baseAbsoluteUrl with fragment is not supported")
+	}
+
+	relativeUrl, err := url.Parse(relativePath)
+
+	if err != nil {
+		return "", err
+	}
+
+	if len(relativeUrl.Query()) > 0 {
+		return "", fmt.Errorf("relativeUrl with query is not supported")
+	}
+
+	if relativeUrl.Fragment != "" {
+		return "", fmt.Errorf("relativeUrl with fragment is not supported")
+	}
+
+	baseUrl.Path = path.Join(baseUrl.Path, relativePath)
+	return baseUrl.String(), nil
 }
