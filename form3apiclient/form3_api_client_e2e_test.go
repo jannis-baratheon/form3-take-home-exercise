b1b12779d280985080f787079bf65ff6c6a9bde6
Janusz <janusz@januszwisniowski.it>
2022-01-07 01:33:08 +0100

    FORM3_API_URL is ony required by the E2E tests

--- form3apiclient/form3_api_client_e2e_test.go
+++ form3apiclient/form3_api_client_e2e_test.go
@@ -1,31 +1,30 @@
 package form3apiclient_test
 
 import (
-	"net/http"
-	"os"
-
 	"github.com/google/uuid"
 	"github.com/jannis-baratheon/Form3-take-home-excercise/form3apiclient"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"net/http"
+	"os"
 )
 
-var _ = Describe("Form3ApiClient with real server", Label("e2e"), func() {
+var _ = Describe("Form3ApiClient with real server", Label("e2e"), Ordered, func() {
 	var accounts form3apiclient.Accounts
-	url := os.Getenv("FORM3_API_URL")
-
-	if url == "" {
-		panic("FORM3_API_URL has to be set")
-	}
 
 	BeforeEach(func() {
+		url := os.Getenv("FORM3_API_URL")
+
+		if url == "" {
+			panic("FORM3_API_URL has to be set")
+		}
 		accounts = form3apiclient.NewForm3APIClient(url, &http.Client{}).Accounts()
 	})
 
-	It("runs simple pipeline without error", func() {
+	It("runs simple pipeline", func() {
 		var resource form3apiclient.AccountData
 
-		By("creating an account", func() {
+		By("create account", func() {
 			accountData := form3apiclient.AccountData{
 				ID:             uuid.NewString(),
 				OrganisationID: uuid.NewString(),
@@ -41,14 +40,14 @@ var _ = Describe("Form3ApiClient with real server", Label("e2e"), func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			DeferCleanup(func() {
-				By("and then deleting it", func() {
+				By("delete account", func() {
 					err := accounts.Delete(resource.ID, 0)
 					Expect(err).NotTo(HaveOccurred())
 				})
 			})
 		})
 
-		By("fetching it afterwards", func() {
+		By("fetch account", func() {
 			fetchedAccountData, err := accounts.Get(resource.ID)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(fetchedAccountData).To(Equal(resource))
